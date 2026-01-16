0f2a2dee2829ad2b8ec9d508ac5e6a17b5e5844f
brooklynprisbrey-duckie <brooklynprisbrey@mail.weber.edu>
2026-01-15 19:26:04 -0700

    Try it out completed in class.

--- src/AbundantNumbers.cpp
+++ src/AbundantNumbers.cpp
@@ -16,26 +16,25 @@ string abundantNum(){
     int numInput = 0;
     int numSum = 0;
     string result;
-    
-    //TODO: Create a loop that will execute the program until the user inputs -1
-        cout << "Enter a number (-1 to quit): ";
-        //TODO: Prompt the user to input a number. Use the numInput variable.
 
-        //TODO: Create the main logic that will calculate if a number is abundant
-        //      Start by creating a for loop that will iterate from 1 to the number
-        //      that the user input.
+    cout << "Enter -1 to quit" << endl;
+    cout << "Enter number to be tested: ";
+    cin >> numInput;
 
-        //TODO: If numInput has a remainder then i is NOT a divisor
-        //      If numInput does NOT have a remainder then add it to numSum
+    while (numInput != -1) {
+        for (int iteratingNum = 1; iteratingNum != numInput; iteratingNum++)
+            if (numInput % iteratingNum == 0) {
+                numSum += iteratingNum;
+                cout << iteratingNum << endl; //scaffolding
+            }
 
-        //TODO: compare numSum to numInput. Using an if-else statement the result variable
-        //      should say one of the following statements:
-        //      result = "a deficient number"
-        //      result = "a perfect number"
-        //      result = "an abundant number"
-        //TODO: Finally, remember to set numSum back to 0
+        if (numSum > numInput) { result = "an abundant number."; }
+        else if (numSum == numInput) { result = "a perfect number."; }
+        else if (numSum == 1) {result = "a prime number! Wow!"; }
+        else { result = "a deficient number."; }
 
-        cout << numInput << " is " << result << endl; 
+        cout << numInput << " is " << result << endl;
+        cout << "The sum of " << numInput << "'s factors is " << numSum << endl;
         //For testing purposes. Please leave in the program
         ss_input << numInput;
         ss_result << result;
@@ -44,6 +43,12 @@ string abundantNum(){
         ss_input.clear(); // Clear any error flags
         ss_result.str("");
         ss_result.clear();
+
+        cout << "Enter -1 to quit" << endl;
+        cout << "Enter number to be tested: ";
+        cin >> numInput;
+        numSum = 0;
+    }
      
     return testResult;
 }
